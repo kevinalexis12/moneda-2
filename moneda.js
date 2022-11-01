86b17586a6060532d1aeb892aa259b9acb0e4496
CrimsonKJ <sgarcia173@misena.edu.co>
2022-11-01 11:19:23 -0500

    moneda

--- moneda.js
+++ moneda.js
@@ -2,20 +2,20 @@ const prompt = require("prompt-sync")();
 const moneda = Math.random();
 
 option = prompt("cara a jugar : ", "");
-apuesta = parseFloat(prompt("valor a apostar : ", ""));
-apoyo = 0;
-
+let apuesta = parseFloat(prompt("valor a apostar : ", 100));
+let apoyo = 2;
 do{
   switch (option) {
     case "cara":
       if (moneda < 0.5) {
         console.log("gano");
            total = apuesta*2
-        console.log(`su apuesta actual es ${total}`);
+           subtotal= total * apoyo;
+        console.log(`la cantidad almacenada es ${total}`);
       } else if (moneda > 0.5) {
         console.log("perdio");
         total = apuesta-apuesta;
-        console.log(`su apuesta actual es ${total}`);
+        console.log(`la cantidad almacenada es ${total}`);
       }
       break;
     case "cruz":
@@ -30,8 +30,7 @@ do{
       }
      
     break;
-   
   }  
- seguir= parseInt(prompt('desea seguir 1. si  2. no :" ',1));
+ seguir= parseInt(prompt('desea seguir 1. si  2. no : ',1));
 } while (seguir==1)
   
